20f1825111b83c28ab71a68ca5731fbcaf40c9c8
snehadaerps <snehadaerpsevol@gmail.com>
2023-12-05 13:23:09 +0530

    added A class java file

---
+++ A.java
@@ -0,0 +1,9 @@
+class A{  
+public static void main(String args[]){  
+  System.out.println("Passed arguments is: ");  
+for(int i=0;i<args.length;i++)  
+	
+System.out.println(args[i]);  
+  
+}  
+}  
